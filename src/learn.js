27b55657e7c96dc2799f617cbe87457553f25ae1
Rafael Mocellin <rafael.mocellin.leszczynski@gmail.com>
2022-09-29 14:05:57 -0300

    create site XPTO

---
+++ src/learn.js
@@ -0,0 +1,30 @@
+function loadPosts() {
+    document.getElementById("posts").innerHTML = 'Carregando...';
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(function (resultado) {
+            return resultado.json();
+        })
+        .then(function (json) {
+            montarBlog(json);
+            
+        })
+        .catch(function (error) {
+        console.log("Deu ruim !!")
+        });
+    
+    
+}
+
+function montarBlog(lista) {
+    let html = '';
+
+    for (let i in lista){
+        html += '<h3>' + lista[i].title + '</h3>';
+        html += lista[i].body + '<br/>';
+        html += '<hr/>';
+        html += '<p/>';
+
+    }
+    document.getElementById("posts").innerHTML = html;
+}
